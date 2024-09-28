3540a40de93cbd81f7a06f9dd7dceb4f44ffe0a7
Jenny Hou <143646348+HouJenny@users.noreply.github.com>
2024-09-27 23:49:32 -0400

    testing ghost spawns

---
+++ Assets/GhostInteraction.ts
@@ -0,0 +1,39 @@
+import { HandInteractor } from "SpectaclesInteractionKit/Core/HandInteractor/HandInteractor";
+import { SIK } from "SpectaclesInteractionKit/SIK";
+
+@component
+export class PinchToSpawn extends BaseScriptComponent {
+    @input
+    objectToSpawn: ObjectPrefab;
+
+    @input
+    leftHandInteractor: HandInteractor;
+
+    @input
+    rightHandInteractor: HandInteractor;
+
+    private handInputData = SIK.HandInputData;
+    private leftHand = this.handInputData.getHand("left");
+    private rightHand = this.handInputData.getHand("right");
+    private spawnedObjects: SceneObject [] = [];
+
+    onAwake() {
+        this.leftHand.onPinchUp(()=> {
+            if (this.leftHandInteractor.targetHitInfo === null) {
+                this.spawnObject(this.leftHand.indexTip.position);
+            }
+        });
+
+        this.rightHand.onPinchUp(()=> {
+            if (this.rightHandInteractor.targetHitInfo === null) {
+                this.spawnObject(this.leftHand.indexTip.position);
+            }
+        });
+    }
+
+    private spawnObject(position: vec3): void {
+        const spawnedObject = this.objectToSpawn.instantiate(this.getSceneObject());
+            spawnedObject.getTransform().setWorldPosition(position);
+            this.spawnedObjects.push(spawnedObject);
+    }
+}
